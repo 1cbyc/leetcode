9e2ec0d1bac408e3458631c11a903005673ed8b1
1cbyc <isaacnsisong@gmail.com>
2024-05-22 02:20:46 +0100

    regular expression matching - answer

---
+++ regular-expression-matching/solution.php
@@ -0,0 +1,39 @@
+class Solution {
+
+/**
+ * @param String $s
+ * @param String $p
+ * @return Boolean
+ */
+function isMatch($s, $p) {
+    $memo = [];
+    // return $this->helper($s, $p, 0, 0, []);
+    return $this->helper($s, $p, 0, 0, $memo);    
+}
+
+function helper($s, $p, $i, $j, &$memo) {
+    if (isset($memo[$i][$j])) {
+        return $memo[$i][$j];
+    }
+
+    if ($j >= strlen($p)) {
+        return $memo [$i][$j] = ($i >= strlen($s));
+    }
+
+    $match = ($i < strlen($s) && ($s[$i] == $p[$j] || $p[$j] == '.'));
+
+    if ($j + 1 < strlen($p) && $p[$j +1] == '*') {
+        $memo[$i][$j] = $this->helper($s, $p, $i, $j +2, $memo) || ($match && $this->helper($s, $p, $i + 1, $j, $memo));            
+    } else {
+        $memo[$i][$j] = $match && $this->helper($s, $p, $i + 1, $j + 1, $memo);
+    }
+
+    return $memo[$i][$j];
+}
+}
+
+// trying to use the examples they gave in the question (it is not needed tho):
+// $solution = new Solution();
+// var_dump($solution->isMatch("aa", "a"));
+// var_dump($solution->isMatch("aa", "a*"));
+// var_dump($solution->isMatch("aa", ".*"));
