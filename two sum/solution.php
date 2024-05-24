2e99931e09ce0ecdae76f067fc288512ac865c3a
1cbyc <isaacnsisong@gmail.com>
2024-05-24 22:38:08 +0100

    two sum -answer

---
+++ two sum/solution.php
@@ -0,0 +1,23 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        num_to_index = {}
+        
+        for i, num in enumerate(nums):
+            complement = target - num
+            
+            if complement in num_to_index:
+                return [num_to_index[complement], i]
+            
+            num_to_index[num] = i
+        
+        return []
+
+solution = Solution()
+print(solution.twoSum([2, 7, 11, 15], 9))
+print(solution.twoSum([3, 2, 4], 6))
+print(solution.twoSum([3, 3], 6))
