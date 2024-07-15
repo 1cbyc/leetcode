8507ac2858b913cd74bc0a53dc61d1a98acd4ed1
1cbyc <isaacnsisong@gmail.com>
2024-07-16 00:22:30 +0100

    solution 154

---
+++ get-the-size-of-a-dataframe/candy/find-minimum-in-rotated-sorted-array-ii/solution.py
@@ -0,0 +1,18 @@
+class Solution:
+    def findMin(self, nums: list[int]) -> int:
+        left, right = 0, len(nums) - 1
+
+        while left < right:
+            mid = (left + right) // 2
+
+            if nums[mid] < nums[right]:
+                right = mid
+            elif nums[mid] > nums[right]:
+                left = mid + 1
+            else:
+                right -= 1
+
+        return nums[left]
+
+
+# the question i solved is https://leetcode.com/problems/find-minimum-in-rotated-sorted-array-ii/
