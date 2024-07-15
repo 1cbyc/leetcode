2bb89dba384fd52da870bc6222a96faa8a813740
1cbyc <isaacnsisong@gmail.com>
2024-07-16 00:20:13 +0100

    solution 2878

--- dungeon-game/solution.py
+++ dungeon-game/solution.py
@@ -1,17 +1,8 @@
-class Solution:
-    def calculateMinimumHP(self, dungeon: List[List[int]]) -> int:
-        m, n = len(dungeon), len(dungeon[0])
+import pandas as pd
+from typing import List
 
-        dp = [[float('inf')] * (n + 1) for _ in range(m + 1)]
+def getDataframeSize(players: pd.DataFrame) -> List[int]:
+    rows, columns = players.shape
+    return [rows, columns]
 
-        dp[m][n - 1] = 1
-        dp[m - 1][n] = 1
-
-        for i in range(m - 1, -1, -1):
-            for j in range(n - 1, -1, -1):
-                needed = max(min(dp[i + 1][j], dp[i][j + 1]) - dungeon[i][j], 1)
-                dp[i][j] = needed
-
-        return dp[0][0]
-
-# the question is https://leetcode.com/problems/dungeon-game/
+# the question i solved is https://leetcode.com/problems/get-the-size-of-a-dataframe/
