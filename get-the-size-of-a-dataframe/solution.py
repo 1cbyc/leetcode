05e985de6ece43a1c8ec2b1f8f582f7c8ee63a25
1cbyc <isaacnsisong@gmail.com>
2024-07-16 00:19:11 +0100

    solution 2878

---
+++ get-the-size-of-a-dataframe/solution.py
@@ -0,0 +1,8 @@
+import pandas as pd
+from typing import List
+
+def getDataframeSize(players: pd.DataFrame) -> List[int]:
+    rows, columns = players.shape
+    return [rows, columns]
+
+the question is https://leetcode.com/problems/get-the-size-of-a-dataframe/
