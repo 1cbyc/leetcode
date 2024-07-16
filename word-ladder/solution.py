d49ae7ad19422216dfa18986d36d707d3c675279
1cbyc <isaacnsisong@gmail.com>
2024-07-16 04:16:48 +0100

    solution 127

---
+++ word-ladder/solution.py
@@ -0,0 +1,39 @@
+from collections import deque
+
+class Solution(object):
+    def ladderLength(self, beginWord, endWord, wordList):
+        """
+        :type beginWord: str
+        :type endWord: str
+        :type wordList: List[str]
+        :rtype: int
+        """
+        if endWord not in wordList:
+            return 0
+
+        wordSet = set(wordList)
+        queue = deque([(beginWord, 1)])
+
+        while queue:
+            current_word, steps = queue.popleft()
+            if current_word == endWord:
+                return steps
+
+            for i in range(len(current_word)):
+                for char in 'abcdefghijklmnopqrstuvwxyz':
+                    next_word = current_word[:i] + char + current_word[i+1:]
+                    if next_word in wordSet:
+                        queue.append((next_word, steps + 1))
+                        wordSet.remove(next_word)
+
+        return 0
+
+solution = Solution()
+beginWord = "hit"
+endWord = "cog"
+wordList = ["hot", "dot", "dog", "lot", "log", "cog"]
+
+print(solution.ladderLength(beginWord, endWord, wordList))
+
+
+# the question i solved is https://leetcode.com/problems/word-ladder/
