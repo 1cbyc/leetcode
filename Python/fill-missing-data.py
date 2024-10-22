c27e83e213617fc952b47cc72cde43e431342681
1cbyc <isaacnsisong@gmail.com>
2024-10-22 11:16:01 +0100

    solving all leetcode problems

---
+++ Python/fill-missing-data.py
@@ -0,0 +1,23 @@
+import pandas as pd
+
+def fillMissingValues(products: pd.DataFrame) -> pd.DataFrame:
+    products['quantity'] = products['quantity'].fillna(0)
+    return products
+
+    products['quantity'].fillna(0, inplace = True)
+    return products
+
+    products.fillna(value={'quantity': 0}, inplace = True)
+    return products
+
+    products.loc[products['quantity'].isnull(),'quantity'] = 0
+    return products
+
+    result = []
+    for i in products.quantity:
+        if pd.isna(i):
+            result.append(0)
+            continue
+        result.append(i)
+    products['quantity'] = result
+    return products
