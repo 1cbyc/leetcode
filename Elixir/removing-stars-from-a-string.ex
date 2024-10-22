a98e6a1e6f83b2bd46b85df78e37f72d56b93e92
1cbyc <isaacnsisong@gmail.com>
2024-10-22 11:25:09 +0100

    solving all leetcode problems

---
+++ Elixir/removing-stars-from-a-string.ex
@@ -0,0 +1,15 @@
+defmodule Solution do
+  @spec remove_stars(s :: String.t) :: String.t
+  def remove_stars(s) do
+    s
+    |> String.split("", trim: true)
+    |> Enum.reverse()
+    |> compute(0, [])
+    |> Enum.join("")
+  end
+
+  defp compute([], _, ret), do: ret # all done
+  defp compute(["*" | s], stars, ret), do: compute(s, stars+1, ret) # increase star count
+  defp compute([c | s], 0, ret), do: compute(s, 0, [c | ret])
+  defp compute([c | s], stars, ret), do: compute(s, stars-1, ret)
+end
