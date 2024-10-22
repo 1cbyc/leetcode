b247fc1583fc3dd6bd6e6cb52d3a8075ca4c8747
1cbyc <isaacnsisong@gmail.com>
2024-10-22 11:15:46 +0100

    solving all leetcode problems

--- Python/change-data-type.py
+++ Python/change-data-type.py
@@ -4,3 +4,4 @@ def changeDatatype(students: pd.DataFrame) -> pd.DataFrame:
     students['grade'] = students['grade'].astype(int)
     return students
 
+# https://leetcode.com/problems/change-data-type/
