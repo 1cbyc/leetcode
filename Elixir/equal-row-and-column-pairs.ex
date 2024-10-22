f6db2b6969c7870db5695eef14dc555b6c1ac784
1cbyc <isaacnsisong@gmail.com>
2024-10-22 11:25:40 +0100

    solving all leetcode problems

---
+++ Elixir/equal-row-and-column-pairs.ex
@@ -0,0 +1,12 @@
+defmodule Solution do
+  @spec equal_pairs(grid :: [[integer]]) :: integer
+  def equal_pairs(grid) do
+    Enum.frequencies(grid)
+    |> then(fn map ->
+      Enum.zip_with(grid, &(&1))
+      |> Enum.map(&(Map.get(map, &1, 0)))
+    end)
+    |> Enum.sum()
+  end
+end
+
