87302dca66bbc3dc436095029dc1b30899384020
1cbyc <ei@nsisong.com>
2025-11-13 10:39:55 +0100

    feat(special-array-i): Add solution.scala

---
+++ special-array-i/solution.scala
@@ -0,0 +1,19 @@
+object Solution {
+  // array is special when adjacent numbers have opposite parity.
+  def isArraySpecial(nums: Array[Int]): Boolean = {
+    if (nums.length <= 1) {
+      true
+    } else {
+      var ok = true
+      var i = 1
+      while (i < nums.length && ok) {
+        if ((nums(i) & 1) == (nums(i - 1) & 1)) {
+          ok = false
+        }
+        i += 1
+      }
+      ok
+    }
+  }
+}
+
