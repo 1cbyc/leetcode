20c7bde9f652b2deaf685da1234042052688b528
1cbyc <ei@nsisong.com>
2025-11-13 11:10:10 +0100

    fix(Python/reshape-data-pivot): Update solution.py

--- Python/reshape-data-pivot/solution.py
+++ Python/reshape-data-pivot/solution.py
@@ -2,4 +2,3 @@ import pandas as pd
 
 def pivotTable(weather: pd.DataFrame) -> pd.DataFrame:
         return weather.pivot_table(index='month', columns='city', values='temperature', aggfunc='max')
-# https://leetcode.com/problems/reshape-data-pivot/
