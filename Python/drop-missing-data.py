6a1d4d6a8edd357156dd20e3dbb98415c3c11dc1
1cbyc <ei@nsisong.com>
2025-11-13 11:10:10 +0100

    fix(Python): Update drop-missing-data.py

--- Python/drop-missing-data.py
+++ Python/drop-missing-data.py
@@ -3,5 +3,3 @@ import pandas as pd
 def dropMissingData(students: pd.DataFrame) -> pd.DataFrame:
     students.dropna(axis = 0, how = "any", subset = "name", inplace = True)
     return students
-
-https://leetcode.com/problems/drop-missing-data/
