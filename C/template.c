21e7b3e89c439ebfb06e0db8ee3352952d00b004
1cbyc <ei@nsisong.com>
2025-11-13 10:57:59 +0100

    fix(C): Update template.c

--- C/template.c
+++ C/template.c
@@ -7,11 +7,6 @@ int solution(/* parameters */) {
 }
 
 void test_solution() {
-    // int result = solution(/* test inputs */);
-    // printf("Test 1: %d\n", result);
-
-    // int result = solution(/* test inputs */);
-    // printf("Test 2: %d\n", result);
 }
 
 int main(int argc, char *argv[]) {
