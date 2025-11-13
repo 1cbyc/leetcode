c8912c919edf0128cb53ba8f58b5e04febfdec01
1cbyc <ei@nsisong.com>
2025-11-13 11:10:10 +0100

    fix(Python): Update drop-duplicate-rows.py

--- Python/drop-duplicate-rows.py
+++ Python/drop-duplicate-rows.py
@@ -3,5 +3,3 @@ import pandas as pd
 def dropDuplicateEmails(customers: pd.DataFrame) -> pd.DataFrame:
     customers.drop_duplicates(subset = "email", keep = "first", inplace = True)
     return customers
-
-# https://leetcode.com/problems/drop-duplicate-rows/
