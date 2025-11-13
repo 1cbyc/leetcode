fbedff9d2e6cc66670a780a2c55bcddbabf696aa
1cbyc <ei@nsisong.com>
2025-11-13 11:10:11 +0100

    fix(create-a-dataframe-from-list): Update solution.py

--- create-a-dataframe-from-list/solution.py
+++ create-a-dataframe-from-list/solution.py
@@ -4,5 +4,3 @@ from typing import List
 def createDataframe(student_data: List[List[int]]) -> pd.DataFrame:
     df = pd.DataFrame(student_data, columns=['student_id', 'age'])
     return df
-
-# this is the question i solved https://leetcode.com/problems/create-a-dataframe-from-list/
