a17d936b05d03beeabfd82a59ecf26b3308c35fc
1cbyc <ei@nsisong.com>
2025-11-13 11:37:56 +0100

    fix(two sum): Update solution.py

--- two sum/solution.py
+++ two sum/solution.py
@@ -1,10 +1,5 @@
 class Solution(object):
     def twoSum(self, nums, target):
-        """
-        :type nums: List[int]
-        :type target: int
-        :rtype: List[int]
-        """
         num_to_index = {}
         
         for i, num in enumerate(nums):
