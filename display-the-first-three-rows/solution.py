1fc458055e53f7cce25b8ab05b9f118956634522
1cbyc <ei@nsisong.com>
2025-11-13 11:10:11 +0100

    fix(display-the-first-three-rows): Update solution.py

--- display-the-first-three-rows/solution.py
+++ display-the-first-three-rows/solution.py
@@ -2,27 +2,3 @@ import pandas as pd
 
 def selectFirstRows(employees: pd.DataFrame) -> pd.DataFrame:
     return employees.head(3)
-
-# DataFrame employees
-# +-------------+-----------+-----------------------+--------+
-# | employee_id | name      | department            | salary |
-# +-------------+-----------+-----------------------+--------+
-# | 3           | Bob       | Operations            | 48675  |
-# | 90          | Alice     | Sales                 | 11096  |
-# | 9           | Tatiana   | Engineering           | 33805  |
-# | 60          | Annabelle | InformationTechnology | 37678  |
-# | 49          | Jonathan  | HumanResources        | 23793  |
-# | 43          | Khaled    | Administration        | 40454  |
-# +-------------+-----------+-----------------------+--------+
-
-# the question solved is: https://leetcode.com/problems/display-the-first-three-rows/description/
-
-# DataFrame: employees
-# +-------------+--------+
-# | Column Name | Type   |
-# +-------------+--------+
-# | employee_id | int    |
-# | name        | object |
-# | department  | object |
-# | salary      | int    |
-# +-------------+--------+
