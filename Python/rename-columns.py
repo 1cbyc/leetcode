6400b6579e23f032144c31fcba53866cfe2a321c
1cbyc <ei@nsisong.com>
2025-11-13 11:10:10 +0100

    fix(Python): Update rename-columns.py

--- Python/rename-columns.py
+++ Python/rename-columns.py
@@ -4,4 +4,3 @@ def renameColumns(students: pd.DataFrame) -> pd.DataFrame:
     students.rename(columns = {"id":"student_id", "first" :"first_name", "last" : "last_name", "age" : "age_in_years"}, inplace = True)
     return students
 
-# https://leetcode.com/problems/rename-columns/
