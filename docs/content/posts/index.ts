5ef8ba336245107845e7a463b440248d629908c4
1cbyc <ei@nsisong.com>
2025-11-13 14:30:33 +0100

    fix(docs/content/posts): Update index.ts

--- docs/content/posts/index.ts
+++ docs/content/posts/index.ts
@@ -1,7 +1,11 @@
 import { maximizeAreaOfSquareHoleInGrid } from "./maximize-area-of-square-hole-in-grid";
+import { maximumSumOfAnHourglass } from "./maximum-sum-of-an-hourglass";
 import { LeetCodePost, byPublishedAtDesc } from "./types";
 
-const registry: LeetCodePost[] = [maximizeAreaOfSquareHoleInGrid];
+const registry: LeetCodePost[] = [
+  maximizeAreaOfSquareHoleInGrid,
+  maximumSumOfAnHourglass,
+];
 
 export const getAllPosts = () => [...registry].sort(byPublishedAtDesc);
 
