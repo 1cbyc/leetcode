77999a4c1f0e936e8dec7819a6b45f63aa1c33a4
1cbyc <ei@nsisong.com>
2025-11-13 10:02:08 +0100

    feat(sort-list): Add solution.cpp

---
+++ sort-list/solution.cpp
@@ -0,0 +1,59 @@
+#include <cstddef>
+
+namespace {
+ListNode *merge(ListNode *a, ListNode *b) {
+    if (!a) {
+        return b;
+    }
+    if (!b) {
+        return a;
+    }
+
+    ListNode dummy;
+    ListNode *tail = &dummy;
+
+    while (a && b) {
+        if (a->val <= b->val) {
+            tail->next = a;
+            a = a->next;
+        } else {
+            tail->next = b;
+            b = b->next;
+        }
+        tail = tail->next;
+    }
+
+    tail->next = a ? a : b;
+    return dummy.next;
+}
+
+ListNode *split(ListNode *head) {
+    ListNode *slow = head;
+    ListNode *fast = head->next;
+
+    while (fast && fast->next) {
+        slow = slow->next;
+        fast = fast->next->next;
+    }
+
+    ListNode *second = slow->next;
+    slow->next = nullptr;
+    return second;
+}
+}  // namespace
+
+class Solution {
+public:
+    ListNode *sortList(ListNode *head) {
+        if (!head || !head->next) {
+            return head;
+        }
+
+        ListNode *second = split(head);
+        ListNode *first_sorted = sortList(head);
+        ListNode *second_sorted = sortList(second);
+
+        return merge(first_sorted, second_sorted);
+    }
+};
+
