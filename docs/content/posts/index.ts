880e7439fe15aadb71d90989db56a3152a7c3803
1cbyc <ei@nsisong.com>
2025-11-18 20:24:31 +0100

    fix(docs/content/posts): Update index.ts

--- docs/content/posts/index.ts
+++ docs/content/posts/index.ts
@@ -1,6 +1,17 @@
 import { maximizeAreaOfSquareHoleInGrid } from "./maximize-area-of-square-hole-in-grid";
 import { maximumSumOfAnHourglass } from "./maximum-sum-of-an-hourglass";
 import { magicSquaresInGrid } from "./magic-squares-in-grid";
+import { arrayWithElementsNotEqualToAverageOfNeighbors } from "./array-with-elements-not-equal-to-average-of-neighbors";
+import { searchA2dMatrix } from "./search-a-2d-matrix";
+import { simplifyPath } from "./simplify-path";
+import { stampingTheGrid } from "./stamping-the-grid";
+import { wiggleSortIi } from "./wiggle-sort-ii";
+import { largestPlusSign } from "./largest-plus-sign";
+import { designNeighborSumService } from "./design-neighbor-sum-service";
+import { frogJump } from "./frog-jump";
+import { lfuCache } from "./lfu-cache";
+import { splitArrayLargestSum } from "./split-array-largest-sum";
+import { stringCompression } from "./string-compression";
 import { collectGeneratedPosts } from "./generated";
 import { LeetCodePost, byPublishedAtDesc } from "./types";
 
@@ -8,6 +19,17 @@ const manualPosts: LeetCodePost[] = [
   maximizeAreaOfSquareHoleInGrid,
   magicSquaresInGrid,
   maximumSumOfAnHourglass,
+  arrayWithElementsNotEqualToAverageOfNeighbors,
+  searchA2dMatrix,
+  simplifyPath,
+  stampingTheGrid,
+  wiggleSortIi,
+  largestPlusSign,
+  designNeighborSumService,
+  frogJump,
+  lfuCache,
+  splitArrayLargestSum,
+  stringCompression,
 ];
 
 const manualSlugSet = new Set(manualPosts.map((post) => post.slug));
