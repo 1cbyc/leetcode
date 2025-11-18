1f44fe4a9a69379407fe8a27afd908014392d809
1cbyc <ei@nsisong.com>
2025-11-18 18:45:58 +0100

    fix(docs): Update next.config.ts

--- docs/next.config.ts
+++ docs/next.config.ts
@@ -1,5 +1,14 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "nsisong.com",
+      },
+    ],
+  },
+};
 
 export default nextConfig;
