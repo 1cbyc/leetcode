baca7c50318ab20249a5748ff649ef809409b2a3
1cbyc <isaacnsisong@gmail.com>
2025-12-09 23:16:41 -0800

    feat(docs\content\posts): Add solving-leetcode-problems-part2.tsx

---
+++ docs/content/posts/solving-leetcode-problems-part2.tsx
@@ -0,0 +1,34 @@
+import { LeetCodePost } from "./types";
+
+export const solvingleetcodeproblemspart2: LeetCodePost = {
+  slug: "solving-leetcode-problems-part2",
+  title: "18 solving leetcode problems part2",
+  summary: "18 solving leetcode problems part2",
+  publishedAt: "2024-01-01",
+  difficulty: "Unknown",
+  languages: ["Python"],
+  tags: ["linked-list"],
+  leetCodeLink: "https://leetcode.com/problems/solving-leetcode-problems-part2/",
+  estimatedReadingMinutes: 1,
+  solutionPaths: [],
+  body: () => (
+    <article className="space-y-6">
+      <section className="space-y-3">
+        <p>--- title: "solving leetcode problems - part 2" description: "welcome to my leetcode problem solving series..." date: "10/17/2024" draft: false ---</p>
+      </section>
+      <section className="space-y-3">
+        <h3 className="text-lg font-semibold">Welcome to my LeetCode Problem Solving Series!</h3>
+        <p>Since this is the second serie, I would be writing about the Add Two Numbers problem</p>
+      </section>
+      <section className="space-y-3">
+        <h3 className="text-lg font-semibold">Read Other Posts In this Series:</h3>
+        <ul className="list-disc space-y-2 pl-5">
+          <li>[Problem 1: Two Sum](https://nsisong.com/blog/17-solving-leetcode-problems/) - Solved using hash maps.</li>
+          <li>[Problem 2: Add Two Numbers](#) - Solved using linked lists.</li>
+          <li>[More problems coming soon...]</li>
+        </ul>
+      </section>
+
+    </article>
+  ),
+};
