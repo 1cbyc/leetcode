c58ffbf614a69fd2e34a056a9442f846dfe9f9b9
1cbyc <isaacnsisong@gmail.com>
2026-04-13 15:53:10 +0100

    Polish 1-easy/problems/0434-number-of-segments-in-a-string solutions

--- 1-easy/problems/0434-number-of-segments-in-a-string/solution.py
+++ 1-easy/problems/0434-number-of-segments-in-a-string/solution.py
@@ -1,11 +1,2 @@
 def countSegments(s):
-    """
-    Count the number of segments (words) in a string.
-
-    Args:
-        s: str - input string
-
-    Returns:
-        int - number of segments
-    """
     return len(s.split())
