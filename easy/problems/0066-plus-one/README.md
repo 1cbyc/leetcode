451936cb6b661b32b962c74986e09fce5ffdffef
1cbyc <isaacnsisong@gmail.com>
2026-04-13 15:46:46 +0100

    Add easy/problems/0066-plus-one solutions

---
+++ easy/problems/0066-plus-one/README.md
@@ -0,0 +1,4 @@
+# 66. Plus One
+
+- LeetCode: https://leetcode.com/problems/plus-one/
+- Languages: cpp, python, typescript
---
+++ easy/problems/0066-plus-one/solution.ts
@@ -0,0 +1,7 @@
+function plusOne(digits: number[]): number[] {
+    for (let i = digits.length - 1; i >= 0; i--) {
+        if (digits[i] < 9) { digits[i]++; return digits; }
+        digits[i] = 0;
+    }
+    return [1, ...digits];
+}
