33dc170bd919f5d100e8c6ce556e3e9b0203ae14
1cbyc <isaacnsisong@gmail.com>
2026-04-13 15:53:09 +0100

    Polish 1-easy/problems/0349-intersection-of-two-arrays solutions

--- 1-easy/problems/0349-intersection-of-two-arrays/solution.py
+++ 1-easy/problems/0349-intersection-of-two-arrays/solution.py
@@ -1,14 +1,4 @@
 def intersection(nums1, nums2):
-    """
-    Find the intersection of two arrays.
-
-    Args:
-        nums1: List[int] - first array
-        nums2: List[int] - second array
-
-    Returns:
-        List[int] - intersection of the two arrays
-    """
     set1 = set(nums1)
     set2 = set(nums2)
     return list(set1 & set2)
