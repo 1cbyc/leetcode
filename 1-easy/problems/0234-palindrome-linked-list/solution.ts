950c75666617ef114e3ae2c04068820c7e27a177
1cbyc <isaacnsisong@gmail.com>
2026-04-13 15:53:09 +0100

    Polish 1-easy/problems/0234-palindrome-linked-list solutions

--- 1-easy/problems/0234-palindrome-linked-list/solution.ts
+++ 1-easy/problems/0234-palindrome-linked-list/solution.ts
@@ -1,12 +1,3 @@
-class ListNode {
-    val: number
-    next: ListNode | null
-    constructor(val?: number, next?: ListNode | null) {
-        this.val = (val===undefined ? 0 : val)
-        this.next = (next===undefined ? null : next)
-    }
-}
-
 function isPalindrome(head: ListNode | null): boolean {
     if (!head || !head.next) return true;
 
