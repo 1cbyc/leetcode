84850f5e25b01fae36558b139bdbb24fa30d5379
1cbyc <isaacnsisong@gmail.com>
2026-04-13 15:53:10 +0100

    Polish 1-easy/problems/0461-hamming-distance solutions

--- 1-easy/problems/0461-hamming-distance/solution.py
+++ 1-easy/problems/0461-hamming-distance/solution.py
@@ -1,13 +1,3 @@
 def hammingDistance(x, y):
-    """
-    Calculate the hamming distance between two integers.
-
-    Args:
-        x: int - first number
-        y: int - second number
-
-    Returns:
-        int - hamming distance (number of differing bits)
-    """
     xor = x ^ y
     return bin(xor).count('1')
