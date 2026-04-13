4dad32590fb4c996c88834e302333cac37f7b90f
1cbyc <isaacnsisong@gmail.com>
2026-04-13 15:53:10 +0100

    Polish 1-easy/problems/0459-repeated-substring-pattern solutions

--- 1-easy/problems/0459-repeated-substring-pattern/solution.py
+++ 1-easy/problems/0459-repeated-substring-pattern/solution.py
@@ -1,11 +1,2 @@
 def repeatedSubstringPattern(s):
-    """
-    Check if a string is composed of a repeated substring.
-
-    Args:
-        s: str - input string
-
-    Returns:
-        bool - true if composed of repeated substring
-    """
     return s in (s + s)[1:-1]
