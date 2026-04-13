a4a0682b191298284073707b4be35e232244ce8d
1cbyc <isaacnsisong@gmail.com>
2026-04-13 15:53:10 +0100

    Polish 1-easy/problems/0448-find-all-numbers-disappeared-in-an-array solutions

--- 1-easy/problems/0448-find-all-numbers-disappeared-in-an-array/solution.py
+++ 1-easy/problems/0448-find-all-numbers-disappeared-in-an-array/solution.py
@@ -1,13 +1,4 @@
 def findDisappearedNumbers(nums):
-    """
-    Find all numbers that disappeared in an array of 1 to n.
-
-    Args:
-        nums: List[int] - array with numbers 1 to n
-
-    Returns:
-        List[int] - disappeared numbers
-    """
     for num in nums:
         index = abs(num) - 1
         nums[index] = -abs(nums[index])
