1ed0d785a68f11f24df698a4d5f1dd85edbcd471
1cbyc <isaacnsisong@gmail.com>
2026-04-13 15:53:10 +0100

    Polish 1-easy/problems/0389-find-the-difference solutions

--- 1-easy/problems/0389-find-the-difference/solution.py
+++ 1-easy/problems/0389-find-the-difference/solution.py
@@ -1,14 +1,4 @@
 def findTheDifference(s, t):
-    """
-    Find the extra character in string t that's not in s.
-
-    Args:
-        s: str - original string
-        t: str - string with one extra character
-
-    Returns:
-        str - the extra character
-    """
     result = 0
 
     for char in s:
