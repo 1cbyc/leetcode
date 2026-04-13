2c7f2b1e01ab650f36121737ce350066813634af
1cbyc <isaacnsisong@gmail.com>
2026-04-13 15:53:09 +0100

    Polish 1-easy/problems/0203-remove-linked-list-elements solutions

--- 1-easy/problems/0203-remove-linked-list-elements/solution.ts
+++ 1-easy/problems/0203-remove-linked-list-elements/solution.ts
@@ -1,12 +1,3 @@
-class ListNode {
-    val: number
-    next: ListNode | null
-    constructor(val?: number, next?: ListNode | null) {
-        this.val = (val===undefined ? 0 : val)
-        this.next = (next===undefined ? null : next)
-    }
-}
-
 function removeElements(head: ListNode | null, val: number): ListNode | null {
     const dummy = new ListNode(0);
     dummy.next = head;
