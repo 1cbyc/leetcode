de1ace46703b939854cc116de8eb400935bf5ef9
1cbyc <isaacnsisong@gmail.com>
2026-04-13 15:53:09 +0100

    Polish 1-easy/problems/0155-min-stack solutions

--- 1-easy/problems/0155-min-stack/solution.py
+++ 1-easy/problems/0155-min-stack/solution.py
@@ -20,7 +20,6 @@ class MinStack:
     def getMin(self) -> int:
         return self.min_stack[-1]
 
-
 # Your MinStack object will be instantiated and called as such:
 # obj = MinStack()
 # obj.push(val)
--- 1-easy/problems/0155-min-stack/solution.ts
+++ 1-easy/problems/0155-min-stack/solution.ts
@@ -24,12 +24,3 @@ class MinStack {
         return this.minStack[this.minStack.length - 1];
     }
 }
-
-/**
- * Your MinStack object will be instantiated and called as such:
- * var obj = new MinStack()
- * obj.push(val)
- * obj.pop()
- * var param_3 = obj.top()
- * var param_4 = obj.getMin()
- */
