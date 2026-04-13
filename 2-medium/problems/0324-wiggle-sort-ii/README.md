6af81b24558fb83525f04129234f14897409614e
1cbyc <isaacnsisong@gmail.com>
2026-04-13 16:11:50 +0100

    Add 2-medium/problems/0324-wiggle-sort-ii solutions

---
+++ 2-medium/problems/0324-wiggle-sort-ii/README.md
@@ -0,0 +1,5 @@
+# 324. Wiggle Sort II
+
+- LeetCode: https://leetcode.com/problems/wiggle-sort-ii/
+- Imported from existing repo work
+- Languages: solution.ts
---
+++ 2-medium/problems/0324-wiggle-sort-ii/solution.ts
@@ -0,0 +1,22 @@
+function wiggleSort(nums: number[]): void {
+  if (nums.length <= 1) {
+    return;
+  }
+
+  const sorted = [...nums].sort((a, b) => a - b);
+  const mid = Math.floor((nums.length + 1) / 2);
+  let left = mid - 1;
+  let right = nums.length - 1;
+
+  for (let i = 0; i < nums.length; ++i) {
+    if (i % 2 === 0) {
+      nums[i] = sorted[left];
+      left -= 1;
+    } else {
+      nums[i] = sorted[right];
+      right -= 1;
+    }
+  }
+}
+
+export { wiggleSort };
