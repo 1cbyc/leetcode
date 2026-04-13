357914a20d57ecc4c77e8e8bb2f956d610a0dc4c
1cbyc <isaacnsisong@gmail.com>
2026-04-13 15:53:09 +0100

    Polish 1-easy/problems/0206-reverse-linked-list solutions

--- 1-easy/problems/0206-reverse-linked-list/solution.ts
+++ 1-easy/problems/0206-reverse-linked-list/solution.ts
@@ -1,12 +1,3 @@
-class ListNode {
-    val: number
-    next: ListNode | null
-    constructor(val?: number, next?: ListNode | null) {
-        this.val = (val===undefined ? 0 : val)
-        this.next = (next===undefined ? null : next)
-    }
-}
-
 function reverseList(head: ListNode | null): ListNode | null {
     let prev: ListNode | null = null;
     let current = head;
