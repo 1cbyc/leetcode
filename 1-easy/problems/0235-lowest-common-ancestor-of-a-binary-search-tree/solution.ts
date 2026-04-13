cd2c262c7d1b890b912640bc3352229d09eb7cbc
1cbyc <isaacnsisong@gmail.com>
2026-04-13 15:53:09 +0100

    Polish 1-easy/problems/0235-lowest-common-ancestor-of-a-binary-search-tree solutions

--- 1-easy/problems/0235-lowest-common-ancestor-of-a-binary-search-tree/solution.ts
+++ 1-easy/problems/0235-lowest-common-ancestor-of-a-binary-search-tree/solution.ts
@@ -1,14 +1,3 @@
-class TreeNode {
-    val: number
-    left: TreeNode | null
-    right: TreeNode | null
-    constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
-        this.val = (val===undefined ? 0 : val)
-        this.left = (left===undefined ? null : left)
-        this.right = (right===undefined ? null : right)
-    }
-}
-
 function lowestCommonAncestor(root: TreeNode | null, p: TreeNode | null, q: TreeNode | null): TreeNode | null {
     while (root) {
         if (p!.val < root.val && q!.val < root.val) {
