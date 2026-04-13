53afbe78cb7052959e7273a3808ea6797a7eeb9b
1cbyc <isaacnsisong@gmail.com>
2026-04-13 15:46:47 +0100

    Add easy/problems/0111-minimum-depth-of-binary-tree solutions

---
+++ easy/problems/0111-minimum-depth-of-binary-tree/README.md
@@ -0,0 +1,4 @@
+# 111. Minimum Depth of Binary Tree
+
+- LeetCode: https://leetcode.com/problems/minimum-depth-of-binary-tree/
+- Languages: cpp, python, typescript
---
+++ easy/problems/0111-minimum-depth-of-binary-tree/solution.ts
@@ -0,0 +1,20 @@
+/**
+ * Definition for a binary tree node.
+ */
+class TreeNode {
+    val: number
+    left: TreeNode | null
+    right: TreeNode | null
+    constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+        this.val = (val===undefined ? 0 : val)
+        this.left = (left===undefined ? null : left)
+        this.right = (right===undefined ? null : right)
+    }
+}
+
+function minDepth(root: TreeNode | null): number {
+    if (!root) return 0;
+    if (!root.left) return 1 + minDepth(root.right);
+    if (!root.right) return 1 + minDepth(root.left);
+    return 1 + Math.min(minDepth(root.left), minDepth(root.right));
+}
