5757b2b5167402ebecb608eb0cfcc26c597118a8
1cbyc <isaacnsisong@gmail.com>
2026-04-13 15:53:08 +0100

    Polish 1-easy/problems/0112-path-sum solutions

--- 1-easy/problems/0112-path-sum/solution.ts
+++ 1-easy/problems/0112-path-sum/solution.ts
@@ -1,17 +1,3 @@
-/**
- * Definition for a binary tree node.
- */
-class TreeNode {
-    val: number
-    left: TreeNode | null
-    right: TreeNode | null
-    constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
-        this.val = (val===undefined ? 0 : val)
-        this.left = (left===undefined ? null : left)
-        this.right = (right===undefined ? null : right)
-    }
-}
-
 function hasPathSum(root: TreeNode | null, targetSum: number): boolean {
     if (!root) return false;
     if (!root.left && !root.right) return root.val === targetSum;
