77fc6f1147e1ddd3d37c63b0e82c6c407da7956b
1cbyc <isaacnsisong@gmail.com>
2026-04-13 15:46:48 +0100

    Add easy/problems/0190-reverse-bits solutions

---
+++ easy/problems/0190-reverse-bits/README.md
@@ -0,0 +1,4 @@
+# 190. Reverse Bits
+
+- LeetCode: https://leetcode.com/problems/reverse-bits/
+- Languages: cpp, python, typescript
---
+++ easy/problems/0190-reverse-bits/solution.ts
@@ -0,0 +1,8 @@
+function reverseBits(n: number): number {
+    let result = 0;
+    for (let i = 0; i < 32; i++) {
+        result = (result << 1) | (n & 1);
+        n >>>= 1;
+    }
+    return result >>> 0;
+}
