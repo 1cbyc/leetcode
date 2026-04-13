4856756ae7b3322c9ec1fb11ecca4ce23e761bcc
1cbyc <isaacnsisong@gmail.com>
2026-04-13 15:53:11 +0100

    Polish 1-easy/problems/0476-number-complement solutions

--- 1-easy/problems/0476-number-complement/solution.py
+++ 1-easy/problems/0476-number-complement/solution.py
@@ -1,13 +1,4 @@
 def findComplement(num):
-    """
-    Find the complement of a number (flip all bits).
-
-    Args:
-        num: int - input number
-
-    Returns:
-        int - complement of the number
-    """
     mask = num
     mask |= mask >> 1
     mask |= mask >> 2
