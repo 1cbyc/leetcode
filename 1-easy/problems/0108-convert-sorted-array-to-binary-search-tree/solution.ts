d89c8fb3f09fed7af7d5d4681507a9e29a8a6732
1cbyc <isaacnsisong@gmail.com>
2026-04-13 15:53:08 +0100

    Polish 1-easy/problems/0108-convert-sorted-array-to-binary-search-tree solutions

--- 1-easy/problems/0108-convert-sorted-array-to-binary-search-tree/solution.ts
+++ 1-easy/problems/0108-convert-sorted-array-to-binary-search-tree/solution.ts
@@ -1,17 +1,3 @@
-/**
- * Definition for a binary tree node.
- */
-class TreeNode {
-    val: number
-    left: TreeNode | null
-    right: TreeNode | null
-    constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
-        this.val = (val===undefined ? 0 : val)
-        this.left = (left===undefined ? null : left)
-        this.right = (right===undefined ? null : right)
-    }
-}
-
 function sortedArrayToBST(nums: number[]): TreeNode | null {
     function buildBST(left: number, right: number): TreeNode | null {
         if (left > right) return null;
