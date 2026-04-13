5c2dc3b916bfcce80517438f745ad4bc686014b1
1cbyc <isaacnsisong@gmail.com>
2026-04-13 15:53:09 +0100

    Polish 1-easy/problems/0344-reverse-string solutions

--- 1-easy/problems/0344-reverse-string/solution.py
+++ 1-easy/problems/0344-reverse-string/solution.py
@@ -1,13 +1,4 @@
 def reverseString(s):
-    """
-    Reverse a string in-place using two pointers.
-
-    Args:
-        s: List[str] - list of characters
-
-    Returns:
-        None (modifies s in-place)
-    """
     left, right = 0, len(s) - 1
     while left < right:
         s[left], s[right] = s[right], s[left]
