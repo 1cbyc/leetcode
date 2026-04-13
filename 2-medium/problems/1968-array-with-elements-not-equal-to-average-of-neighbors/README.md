96bd5b83fb6d11f6f3cb49a7a15e3655ebd11af5
1cbyc <isaacnsisong@gmail.com>
2026-04-13 16:11:51 +0100

    Add 2-medium/problems/1968-array-with-elements-not-equal-to-average-of-neighbors solutions

---
+++ 2-medium/problems/1968-array-with-elements-not-equal-to-average-of-neighbors/README.md
@@ -0,0 +1,5 @@
+# 1968. Array With Elements Not Equal to Average of Neighbors
+
+- LeetCode: https://leetcode.com/problems/array-with-elements-not-equal-to-average-of-neighbors/
+- Imported from existing repo work
+- Languages: solution.ts
---
+++ 2-medium/problems/1968-array-with-elements-not-equal-to-average-of-neighbors/solution.ts
@@ -0,0 +1,25 @@
+function rearrangeArray(nums: number[]): number[] {
+  if (nums.length <= 2) {
+    return [...nums];
+  }
+
+  const sorted = [...nums].sort((a, b) => a - b);
+  const mid = Math.floor((nums.length + 1) / 2);
+  let left = mid - 1;
+  let right = nums.length - 1;
+  const result: number[] = [];
+
+  for (let i = 0; i < nums.length; ++i) {
+    if (i % 2 === 0) {
+      result.push(sorted[left]);
+      left -= 1;
+    } else {
+      result.push(sorted[right]);
+      right -= 1;
+    }
+  }
+
+  return result;
+}
+
+export { rearrangeArray };
