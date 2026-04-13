16f08a00aecf187a120798be162ddf4fa844cae9
1cbyc <isaacnsisong@gmail.com>
2026-04-13 15:53:09 +0100

    Polish 1-easy/problems/0257-binary-tree-paths solutions

--- 1-easy/problems/0257-binary-tree-paths/solution.ts
+++ 1-easy/problems/0257-binary-tree-paths/solution.ts
@@ -1,14 +1,3 @@
-class TreeNode {
-    val: number
-    left: TreeNode | null
-    right: TreeNode | null
-    constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
-        this.val = (val===undefined ? 0 : val)
-        this.left = (left===undefined ? null : left)
-        this.right = (right===undefined ? null : right)
-    }
-}
-
 function binaryTreePaths(root: TreeNode | null): string[] {
     const result: string[] = [];
 
