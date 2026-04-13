a15cb5c6951cbbaec8a130cc809b3b2723d854be
1cbyc <isaacnsisong@gmail.com>
2026-04-13 15:53:10 +0100

    Polish 1-easy/problems/0414-third-maximum-number solutions

--- 1-easy/problems/0414-third-maximum-number/solution.py
+++ 1-easy/problems/0414-third-maximum-number/solution.py
@@ -1,13 +1,4 @@
 def thirdMax(nums):
-    """
-    Find the third maximum number in an array.
-
-    Args:
-        nums: List[int] - input array
-
-    Returns:
-        int - third maximum or maximum if less than 3 unique numbers
-    """
     unique_nums = list(set(nums))
 
     if len(unique_nums) < 3:
