bb98206cbe8406849f87220b9dde8e3d161f32c9
1cbyc <isaacnsisong@gmail.com>
2026-04-13 15:53:09 +0100

    Polish 1-easy/problems/0222-count-complete-tree-nodes solutions

--- 1-easy/problems/0222-count-complete-tree-nodes/solution.ts
+++ 1-easy/problems/0222-count-complete-tree-nodes/solution.ts
@@ -1,17 +1,3 @@
-/**
- * Definition for a binary tree node.
- * class TreeNode {
- *     val: number
- *     left: TreeNode | null
- *     right: TreeNode | null
- *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
- *         this.val = (val === undefined ? 0 : val)
- *         this.left = (left === undefined ? null : left)
- *         this.right = (right === undefined ? null : right)
- *     }
- * }
- */
-
 function countNodes(root: TreeNode | null): number {
     const getLeftHeight = (node: TreeNode | null): number => {
         let height = 0;
