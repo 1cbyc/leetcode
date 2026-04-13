2c39806668045d240c97064db7e1ee14c4d2cbb4
1cbyc <isaacnsisong@gmail.com>
2026-04-13 15:46:46 +0100

    Add easy/problems/0058-length-of-last-word solutions

---
+++ easy/problems/0058-length-of-last-word/README.md
@@ -0,0 +1,4 @@
+# 58. Length of Last Word
+
+- LeetCode: https://leetcode.com/problems/length-of-last-word/
+- Languages: cpp, python, typescript
---
+++ easy/problems/0058-length-of-last-word/solution.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    int lengthOfLastWord(string s) {
+        int len = 0, i = s.size()-1;
+        while (i >= 0 && s[i] == ' ') i--;
+        while (i >= 0 && s[i] != ' ') { len++; i--; }
+        return len;
+    }
+};
