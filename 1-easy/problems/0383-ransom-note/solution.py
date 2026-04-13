7d9150fe7542847e42886617e3ba98e579892038
1cbyc <isaacnsisong@gmail.com>
2026-04-13 15:53:10 +0100

    Polish 1-easy/problems/0383-ransom-note solutions

--- 1-easy/problems/0383-ransom-note/solution.py
+++ 1-easy/problems/0383-ransom-note/solution.py
@@ -1,14 +1,4 @@
 def canConstruct(ransomNote, magazine):
-    """
-    Determine if ransomNote can be constructed from magazine.
-
-    Args:
-        ransomNote: str - the target note
-        magazine: str - the source of characters
-
-    Returns:
-        bool - true if ransomNote can be constructed
-    """
     from collections import Counter
 
     ransom_count = Counter(ransomNote)
