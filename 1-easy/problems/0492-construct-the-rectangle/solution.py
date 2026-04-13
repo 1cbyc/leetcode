b3d165a6ee8b3dc7c971fbf40f081db1e8ed4349
1cbyc <isaacnsisong@gmail.com>
2026-04-13 15:53:11 +0100

    Polish 1-easy/problems/0492-construct-the-rectangle solutions

--- 1-easy/problems/0492-construct-the-rectangle/solution.py
+++ 1-easy/problems/0492-construct-the-rectangle/solution.py
@@ -1,13 +1,4 @@
 def constructRectangle(area):
-    """
-    Find dimensions of rectangle with given area closest to square.
-
-    Args:
-        area: int - area of the rectangle
-
-    Returns:
-        List[int] - [length, width] where length >= width
-    """
     w = int(area ** 0.5)
 
     while area % w != 0:
