bac4b1dd5adae31254ec9bb07e5b440846e40602
1cbyc <isaacnsisong@gmail.com>
2026-06-14 14:41:52 +0100

    Add 1-easy/problems/2913-subarrays-distinct-element-sum-of-squares-i solutions

---
+++ 1-easy/problems/2913-subarrays-distinct-element-sum-of-squares-i/README.md
@@ -0,0 +1,5 @@
+# 2913. Subarrays Distinct Element Sum of Squares I
+
+- LeetCode: https://leetcode.com/problems/subarrays-distinct-element-sum-of-squares-i/
+- Imported from easy backlog
+- Languages: cpp, python, typescript
---
+++ 1-easy/problems/2913-subarrays-distinct-element-sum-of-squares-i/solution.ts
@@ -0,0 +1,12 @@
+function sumCounts(nums: any): boolean | number | string | any {
+  """
+  :type nums: List[int]
+  :rtype: int
+  """
+  MOD = 10**9+7
+  class BIT(object):  # 0-indexed.
+      def __init__(self, n):
+          self.__bit = [0]*(n+1)  # Extra one for dummy node.
+}
+
+export { sumCounts };
