a83590d073f2957cf8791d6bdca579e25f68105f
1cbyc <isaacnsisong@gmail.com>
2026-06-14 14:42:03 +0100

    Add 1-easy/problems/3754-concatenate-non-zero-digits-and-multiply-by-sum-i solutions

---
+++ 1-easy/problems/3754-concatenate-non-zero-digits-and-multiply-by-sum-i/README.md
@@ -0,0 +1,5 @@
+# 3754. Concatenate Non-Zero Digits and Multiply by Sum I
+
+- LeetCode: https://leetcode.com/problems/concatenate-non-zero-digits-and-multiply-by-sum-i/
+- Imported from easy backlog
+- Languages: cpp, python, typescript
---
+++ 1-easy/problems/3754-concatenate-non-zero-digits-and-multiply-by-sum-i/solution.py
@@ -0,0 +1,20 @@
+class Solution:
+    def sumAndMultiply(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        def reverse(n):
+            result = 0
+            while n:
+                n, r = divmod(n, 10)
+                result = result*10+r
+            return result
+
+        total = x = 0
+        while n:
+            n, r = divmod(n, 10)
+            total += r
+            if r:
+                x = x*10+r
+        return reverse(x)*total
