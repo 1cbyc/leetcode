e92e4b2fd17ccd4eb5800ec31f0a0a3fa607b0a9
1cbyc <isaacnsisong@gmail.com>
2026-06-14 14:41:47 +0100

    Add 1-easy/problems/2485-find-the-pivot-integer solutions

---
+++ 1-easy/problems/2485-find-the-pivot-integer/README.md
@@ -0,0 +1,5 @@
+# 2485. Find the Pivot Integer
+
+- LeetCode: https://leetcode.com/problems/find-the-pivot-integer/
+- Imported from easy backlog
+- Languages: cpp, python, typescript
---
+++ 1-easy/problems/2485-find-the-pivot-integer/solution.ts
@@ -0,0 +1,10 @@
+function pivotInteger(n: any): boolean | number | string | any {
+  """
+  :type n: int
+  :rtype: int
+  """
+  x = int(((n+1)*n//2)**0.5+0.5)
+  return x if x**2 == (n+1)*n//2 else -1;
+}
+
+export { pivotInteger };
