619f6b1abcb477909ee24626ce64c233632ec82d
1cbyc <isaacnsisong@gmail.com>
2026-06-14 14:41:31 +0100

    Add 1-easy/problems/1232-check-if-it-is-a-straight-line solutions

---
+++ 1-easy/problems/1232-check-if-it-is-a-straight-line/README.md
@@ -0,0 +1,5 @@
+# 1232. Check If It Is a Straight Line
+
+- LeetCode: https://leetcode.com/problems/check-if-it-is-a-straight-line/
+- Imported from easy backlog
+- Languages: cpp, python, typescript
---
+++ 1-easy/problems/1232-check-if-it-is-a-straight-line/solution.py
@@ -0,0 +1,13 @@
+from typing import List, Optional, Dict, Set, Tuple
+
+class Solution:
+    def checkStraightLine(self, coordinates):
+        """
+        :type coordinates: List[List[int]]
+        :rtype: bool
+        """
+        i, j = coordinates[:2]
+        return all(i[0] * j[1] - j[0] * i[1] +
+                   j[0] * k[1] - k[0] * j[1] +
+                   k[0] * i[1] - i[0] * k[1] == 0
+                   for k in coordinates)
