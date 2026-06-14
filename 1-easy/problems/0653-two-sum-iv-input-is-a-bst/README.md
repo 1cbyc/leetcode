1ef17dc7475e0ce8e1d45a3e366789ac53869a3b
1cbyc <isaacnsisong@gmail.com>
2026-06-14 14:41:25 +0100

    Add 1-easy/problems/0653-two-sum-iv-input-is-a-bst solutions

---
+++ 1-easy/problems/0653-two-sum-iv-input-is-a-bst/README.md
@@ -0,0 +1,5 @@
+# 653. Two Sum IV - Input is a BST
+
+- LeetCode: https://leetcode.com/problems/two-sum-iv-input-is-a-bst/
+- Imported from easy backlog
+- Languages: cpp, python, typescript
---
+++ 1-easy/problems/0653-two-sum-iv-input-is-a-bst/solution.ts
@@ -0,0 +1,16 @@
+function findTarget(root: any, k: any): boolean | number | string | any {
+  """
+  :type root: TreeNode
+  :type k: int
+  :rtype: bool
+  """
+  class BSTIterator(object):
+      def __init__(self, root, forward):
+          self.__node = root
+          self.__forward = forward
+          self.__s = []
+          self.__cur = null
+          self.next()
+}
+
+export { findTarget };
