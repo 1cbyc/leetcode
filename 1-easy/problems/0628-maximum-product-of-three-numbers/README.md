98e3c823fc73e783287d107730e74bac8fdd2020
1cbyc <isaacnsisong@gmail.com>
2026-06-14 14:41:24 +0100

    Add 1-easy/problems/0628-maximum-product-of-three-numbers solutions

---
+++ 1-easy/problems/0628-maximum-product-of-three-numbers/README.md
@@ -0,0 +1,5 @@
+# 628. Maximum Product of Three Numbers
+
+- LeetCode: https://leetcode.com/problems/maximum-product-of-three-numbers/
+- Imported from easy backlog
+- Languages: cpp, python, typescript
---
+++ 1-easy/problems/0628-maximum-product-of-three-numbers/solution.ts
@@ -0,0 +1,10 @@
+function maximumProduct(nums: any): boolean | number | string | any {
+  """
+  :type nums: List[int]
+  :rtype: int
+  """
+  min1, min2 = float("inf"), float("inf")
+  max1, max2, max3 = float("-inf"), float("-inf"), float("-inf")
+}
+
+export { maximumProduct };
