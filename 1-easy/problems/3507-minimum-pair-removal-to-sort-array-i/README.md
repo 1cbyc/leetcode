a5be1a8765bb9018a2b1235d16cc52c14122e6cb
1cbyc <isaacnsisong@gmail.com>
2026-06-14 14:42:00 +0100

    Add 1-easy/problems/3507-minimum-pair-removal-to-sort-array-i solutions

---
+++ 1-easy/problems/3507-minimum-pair-removal-to-sort-array-i/README.md
@@ -0,0 +1,5 @@
+# 3507. Minimum Pair Removal to Sort Array I
+
+- LeetCode: https://leetcode.com/problems/minimum-pair-removal-to-sort-array-i/
+- Imported from easy backlog
+- Languages: cpp, python, typescript
---
+++ 1-easy/problems/3507-minimum-pair-removal-to-sort-array-i/solution.ts
@@ -0,0 +1,13 @@
+function minimumPairRemoval(nums: any): boolean | number | string | any {
+  """
+  :type nums: List[int]
+  :rtype: int
+  """
+  def add(i):
+      if (0 <= i < right[i] < (nums).length) {
+          sl.add([nums[i]+nums[right[i]], i])
+          if (nums[i] > nums[right[i]]) {
+              cnt[0] += 1
+}
+
+export { minimumPairRemoval };
