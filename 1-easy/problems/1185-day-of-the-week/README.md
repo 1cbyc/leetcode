cb28cdc11217e38d5526424cfb88fece76618baf
1cbyc <isaacnsisong@gmail.com>
2026-06-14 14:41:30 +0100

    Add 1-easy/problems/1185-day-of-the-week solutions

---
+++ 1-easy/problems/1185-day-of-the-week/README.md
@@ -0,0 +1,5 @@
+# 1185. Day of the Week
+
+- LeetCode: https://leetcode.com/problems/day-of-the-week/
+- Imported from easy backlog
+- Languages: cpp, python, typescript
---
+++ 1-easy/problems/1185-day-of-the-week/solution.ts
@@ -0,0 +1,12 @@
+function dayOfTheWeek(day: any, month: any, year: any): boolean | number | string | any {
+  """
+  :type day: int
+  :type month: int
+  :type year: int
+  :rtype: str
+  """
+  DAYS = ["Sunday", "Monday", "Tuesday", "Wednesday", \
+          "Thursday", "Friday", "Saturday"]
+}
+
+export { dayOfTheWeek };
