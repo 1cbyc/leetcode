c34aec0c9eb5ccfcb770bfed0324b371ad5efade
1cbyc <isaacnsisong@gmail.com>
2026-06-14 14:42:01 +0100

    Add 1-easy/problems/3602-hexadecimal-and-hexatrigesimal-conversion solutions

---
+++ 1-easy/problems/3602-hexadecimal-and-hexatrigesimal-conversion/README.md
@@ -0,0 +1,5 @@
+# 3602. Hexadecimal and Hexatrigesimal Conversion
+
+- LeetCode: https://leetcode.com/problems/hexadecimal-and-hexatrigesimal-conversion/
+- Imported from easy backlog
+- Languages: cpp, python, typescript
---
+++ 1-easy/problems/3602-hexadecimal-and-hexatrigesimal-conversion/solution.ts
@@ -0,0 +1,18 @@
+function concatHex36(n: any): boolean | number | string | any {
+  """
+  :type n: int
+  :rtype: str
+  """
+  LOOKUP = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+  def convert(n, l):
+      result = []
+      while (n) {
+          n, r = divmod(n, l)
+          result.append(LOOKUP[r])
+      result.reverse()
+      return "".join(result);
+
+  return convert(n**2, 16)+convert(n**3, 36);
+}
+
+export { concatHex36 };
