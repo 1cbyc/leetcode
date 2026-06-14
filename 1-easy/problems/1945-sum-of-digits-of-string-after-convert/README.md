2e99153757dab9e0503fff9c4905cc9cb562e463
1cbyc <isaacnsisong@gmail.com>
2026-06-14 14:41:40 +0100

    Add 1-easy/problems/1945-sum-of-digits-of-string-after-convert solutions

---
+++ 1-easy/problems/1945-sum-of-digits-of-string-after-convert/README.md
@@ -0,0 +1,5 @@
+# 1945. Sum of Digits of String After Convert
+
+- LeetCode: https://leetcode.com/problems/sum-of-digits-of-string-after-convert/
+- Imported from easy backlog
+- Languages: cpp, python, typescript
---
+++ 1-easy/problems/1945-sum-of-digits-of-string-after-convert/solution.py
@@ -0,0 +1,16 @@
+class Solution:
+    def getLucky(self, s, k):
+        """
+        :type s: str
+        :type k: int
+        :rtype: int
+        """
+        total = reduce(lambda total, x: total+sum(divmod((ord(x)-ord('a')+1), 10)), s, 0)
+        while k > 1 and total > 9:
+            new_total = 0
+            while total:
+                total, x = divmod(total, 10)
+                new_total += x
+            total = new_total
+            k -= 1
+        return total
