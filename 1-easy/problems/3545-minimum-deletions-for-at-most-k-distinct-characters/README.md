71e9deac824d9f31ea54e62e347ceb1bfa9ac2de
1cbyc <isaacnsisong@gmail.com>
2026-06-14 14:42:00 +0100

    Add 1-easy/problems/3545-minimum-deletions-for-at-most-k-distinct-characters solutions

---
+++ 1-easy/problems/3545-minimum-deletions-for-at-most-k-distinct-characters/README.md
@@ -0,0 +1,5 @@
+# 3545. Minimum Deletions for At Most K Distinct Characters
+
+- LeetCode: https://leetcode.com/problems/minimum-deletions-for-at-most-k-distinct-characters/
+- Imported from easy backlog
+- Languages: cpp, python, typescript
---
+++ 1-easy/problems/3545-minimum-deletions-for-at-most-k-distinct-characters/solution.py
@@ -0,0 +1,22 @@
+class Solution:
+    def minDeletion(self, s, k):
+        """
+        :type s: str
+        :type k: int
+        :rtype: int
+        """
+        cnt = [0]*26
+        for x in s:
+            cnt[ord(x)-ord('a')] += 1
+        cnt2 = [0]*(max(cnt)+1)
+        for x in cnt:
+            cnt2[x] += 1
+        result = 0
+        total = 26-k
+        for i, x in enumerate(cnt2):
+            c = min(total, x)
+            result += i*c
+            total -= c
+            if total == 0:
+                break
+        return result
