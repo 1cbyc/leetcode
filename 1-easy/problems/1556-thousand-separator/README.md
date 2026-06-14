67a226a636c3c3aa855214850d32063271b132cf
1cbyc <isaacnsisong@gmail.com>
2026-06-14 14:41:35 +0100

    Add 1-easy/problems/1556-thousand-separator solutions

---
+++ 1-easy/problems/1556-thousand-separator/README.md
@@ -0,0 +1,5 @@
+# 1556. Thousand Separator
+
+- LeetCode: https://leetcode.com/problems/thousand-separator/
+- Imported from easy backlog
+- Languages: cpp, python, typescript
---
+++ 1-easy/problems/1556-thousand-separator/solution.ts
@@ -0,0 +1,15 @@
+function thousandSeparator(n: any): boolean | number | string | any {
+  """
+  :type n: int
+  :rtype: str
+  """
+  result = []
+  s = str(n)
+  for (i, c in enumerate(str(n))) {
+      if (i && ((s).length-i)%3 == 0) {
+          result.append(".")
+      result.append(c)
+  return "".join(result);
+}
+
+export { thousandSeparator };
