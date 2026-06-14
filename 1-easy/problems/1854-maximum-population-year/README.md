df6da3f5789e5a2175da08170b4ccbdae909c15b
1cbyc <isaacnsisong@gmail.com>
2026-06-14 14:41:39 +0100

    Add 1-easy/problems/1854-maximum-population-year solutions

---
+++ 1-easy/problems/1854-maximum-population-year/README.md
@@ -0,0 +1,5 @@
+# 1854. Maximum Population Year
+
+- LeetCode: https://leetcode.com/problems/maximum-population-year/
+- Imported from easy backlog
+- Languages: cpp, python, typescript
---
+++ 1-easy/problems/1854-maximum-population-year/solution.ts
@@ -0,0 +1,20 @@
+function maximumPopulation(logs: any): boolean | number | string | any {
+  """
+  :type logs: List[List[int]]
+  :rtype: int
+  """
+  MIN_YEAR, MAX_YEAR = 1950, 2050
+  years = [0]*(MAX_YEAR-MIN_YEAR+1)
+  for (s, e in logs) {
+      years[s-MIN_YEAR] += 1
+      years[e-MIN_YEAR] -= 1
+  result = 0
+  for (i in Array.from({length: (years}, (_, i) => i).length)) {
+      if (i) {
+          years[i] += years[i-1]
+      if (years[i] > years[result]) {
+          result = i
+  return result+MIN_YEAR;
+}
+
+export { maximumPopulation };
