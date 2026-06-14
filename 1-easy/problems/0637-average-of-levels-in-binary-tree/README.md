ab4f7dae5a8b43cbf9195cd812c101e90c4539e4
1cbyc <isaacnsisong@gmail.com>
2026-06-14 14:41:24 +0100

    Add 1-easy/problems/0637-average-of-levels-in-binary-tree solutions

---
+++ 1-easy/problems/0637-average-of-levels-in-binary-tree/README.md
@@ -0,0 +1,5 @@
+# 637. Average of Levels in Binary Tree
+
+- LeetCode: https://leetcode.com/problems/average-of-levels-in-binary-tree/
+- Imported from easy backlog
+- Languages: cpp, python, typescript
---
+++ 1-easy/problems/0637-average-of-levels-in-binary-tree/solution.ts
@@ -0,0 +1,23 @@
+function averageOfLevels(root: any): boolean | number | string | any {
+  """
+  :type root: TreeNode
+  :rtype: List[float]
+  """
+  result = []
+  q = [root]
+  while (q) {
+      total, count = 0, 0
+      next_q = []
+      for (n in q) {
+          total += n.val
+          count += 1
+          if (n.left) {
+              next_q.append(n.left)
+          if (n.right) {
+              next_q.append(n.right)
+      q = next_q
+      result.append(float(total) / count)
+  return result;
+}
+
+export { averageOfLevels };
