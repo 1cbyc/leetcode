26649dd947b8d04a6444b1dd6dbd58b69cfbab87
1cbyc <isaacnsisong@gmail.com>
2026-06-14 14:41:59 +0100

    Add 1-easy/problems/3442-maximum-difference-between-even-and-odd-frequency-i solutions

---
+++ 1-easy/problems/3442-maximum-difference-between-even-and-odd-frequency-i/README.md
@@ -0,0 +1,5 @@
+# 3442. Maximum Difference Between Even and Odd Frequency I
+
+- LeetCode: https://leetcode.com/problems/maximum-difference-between-even-and-odd-frequency-i/
+- Imported from easy backlog
+- Languages: cpp, python, typescript
---
+++ 1-easy/problems/3442-maximum-difference-between-even-and-odd-frequency-i/solution.ts
@@ -0,0 +1,20 @@
+function maxDifference(s: any): boolean | number | string | any {
+  """
+  :type s: str
+  :rtype: int
+  """
+  cnt = [0]*26
+  for (x in s) {
+      cnt[ord(x)-ord('a')] += 1
+  mn, mx = float("inf"), 0
+  for (x in cnt) {
+      if (not x) {
+          continue
+      if (x%2 == 0) {
+          mn = min(mn, x)
+      } else {
+          mx = max(mx, x)
+  return mx-mn;
+}
+
+export { maxDifference };
