b694aacda2ab1aeae819ffbee652a05de9deaca9
1cbyc <isaacnsisong@gmail.com>
2026-06-14 14:41:45 +0100

    Add 1-easy/problems/2383-minimum-hours-of-training-to-win-a-competition solutions

---
+++ 1-easy/problems/2383-minimum-hours-of-training-to-win-a-competition/README.md
@@ -0,0 +1,5 @@
+# 2383. Minimum Hours of Training to Win a Competition
+
+- LeetCode: https://leetcode.com/problems/minimum-hours-of-training-to-win-a-competition/
+- Imported from easy backlog
+- Languages: cpp, python, typescript
---
+++ 1-easy/problems/2383-minimum-hours-of-training-to-win-a-competition/solution.py
@@ -0,0 +1,19 @@
+from typing import List, Optional, Dict, Set, Tuple
+
+class Solution:
+    def minNumberOfHours(self, initialEnergy, initialExperience, energy, experience):
+        """
+        :type initialEnergy: int
+        :type initialExperience: int
+        :type energy: List[int]
+        :type experience: List[int]
+        :rtype: int
+        """
+        result = 0
+        for hp, ex in itertools.izip(energy, experience):
+            inc1 = max((hp+1)-initialEnergy, 0)
+            inc2 = max((ex+1)-initialExperience, 0)
+            result += inc1+inc2
+            initialEnergy += inc1-hp
+            initialExperience += inc2+ex
+        return result
