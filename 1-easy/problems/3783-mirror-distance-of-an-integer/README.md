c25fe96a59c8bea9f568040f26e9c44d45434888
1cbyc <isaacnsisong@gmail.com>
2026-06-14 14:42:03 +0100

    Add 1-easy/problems/3783-mirror-distance-of-an-integer solutions

---
+++ 1-easy/problems/3783-mirror-distance-of-an-integer/README.md
@@ -0,0 +1,5 @@
+# 3783. Mirror Distance of an Integer
+
+- LeetCode: https://leetcode.com/problems/mirror-distance-of-an-integer/
+- Imported from easy backlog
+- Languages: cpp, python, typescript
---
+++ 1-easy/problems/3783-mirror-distance-of-an-integer/solution.py
@@ -0,0 +1,14 @@
+class Solution:
+    def mirrorDistance(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        def reverse(n):
+            result = 0
+            while n:
+                n, r = divmod(n, 10)
+                result = result*10+r
+            return result
+
+        return abs(n-reverse(n))
