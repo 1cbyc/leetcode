47c06d601167163f2f67cc49f5360819e7ff2186
1cbyc <isaacnsisong@gmail.com>
2026-06-14 14:41:55 +0100

    Add 1-easy/problems/3114-latest-time-you-can-obtain-after-replacing-characters solutions

---
+++ 1-easy/problems/3114-latest-time-you-can-obtain-after-replacing-characters/README.md
@@ -0,0 +1,5 @@
+# 3114. Latest Time You Can Obtain After Replacing Characters
+
+- LeetCode: https://leetcode.com/problems/latest-time-you-can-obtain-after-replacing-characters/
+- Imported from easy backlog
+- Languages: cpp, python, typescript
---
+++ 1-easy/problems/3114-latest-time-you-can-obtain-after-replacing-characters/solution.py
@@ -0,0 +1,16 @@
+class Solution:
+    def findLatestTime(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        result = list(s)
+        if result[0] == '?': 
+            result[0] = '1' if result[1] == '?' or result[1] <= '1' else '0'
+        if result[1] == '?': 
+            result[1] = '1' if result[0] == '1' else '9'
+        if result[3] == '?':
+            result[3] = '5'
+        if result[4] == '?':
+            result[4] = '9'
+        return "".join(result)
