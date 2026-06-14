f1ba59916b9251364fd5fd1fffd8b93ecdae2783
1cbyc <isaacnsisong@gmail.com>
2026-06-14 14:41:26 +0100

    Add 1-easy/problems/0733-flood-fill solutions

---
+++ 1-easy/problems/0733-flood-fill/README.md
@@ -0,0 +1,5 @@
+# 733. Flood Fill
+
+- LeetCode: https://leetcode.com/problems/flood-fill/
+- Imported from easy backlog
+- Languages: cpp, python, typescript
---
+++ 1-easy/problems/0733-flood-fill/solution.ts
@@ -0,0 +1,12 @@
+function floodFill(image: any, sr: any, sc: any, newColor: any): boolean | number | string | any {
+  """
+  :type image: List[List[int]]
+  :type sr: int
+  :type sc: int
+  :type newColor: int
+  :rtype: List[List[int]]
+  """
+  directions = [(0, -1), (0, 1), (-1, 0), (1, 0)]
+}
+
+export { floodFill };
