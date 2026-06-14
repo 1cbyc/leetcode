b4eab4ebbe19aca632cf4a159db68c82ba990996
1cbyc <isaacnsisong@gmail.com>
2026-06-14 14:41:43 +0100

    Add 1-easy/problems/2224-minimum-number-of-operations-to-convert-time solutions

---
+++ 1-easy/problems/2224-minimum-number-of-operations-to-convert-time/README.md
@@ -0,0 +1,5 @@
+# 2224. Minimum Number of Operations to Convert Time
+
+- LeetCode: https://leetcode.com/problems/minimum-number-of-operations-to-convert-time/
+- Imported from easy backlog
+- Languages: cpp, python, typescript
---
+++ 1-easy/problems/2224-minimum-number-of-operations-to-convert-time/solution.ts
@@ -0,0 +1,16 @@
+function convertTime(current: any, correct: any): boolean | number | string | any {
+  """
+  :type current: str
+  :type correct: str
+  :rtype: int
+  """
+  OPS = (60, 15, 5, 1)
+  diff = (int(correct[:2])*60+int(correct[3:]))-(int(current[:2])*60+int(current[3:]))
+  result = 0
+  for (x in OPS) {
+      q, diff = divmod(diff, x)
+      result += q
+  return result;
+}
+
+export { convertTime };
