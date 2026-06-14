e9852ac55e5597756bee6ee463c1e96e3772a84f
1cbyc <isaacnsisong@gmail.com>
2026-06-14 14:41:44 +0100

    Add 1-easy/problems/2269-find-the-k-beauty-of-a-number solutions

---
+++ 1-easy/problems/2269-find-the-k-beauty-of-a-number/README.md
@@ -0,0 +1,5 @@
+# 2269. Find the K-Beauty of a Number
+
+- LeetCode: https://leetcode.com/problems/find-the-k-beauty-of-a-number/
+- Imported from easy backlog
+- Languages: cpp, python, typescript
---
+++ 1-easy/problems/2269-find-the-k-beauty-of-a-number/solution.ts
@@ -0,0 +1,19 @@
+function divisorSubstrings(num: any, k: any): boolean | number | string | any {
+  """
+  :type num: int
+  :type k: int
+  :rtype: int
+  """
+  result = curr = 0
+  s = map(int, str(num))
+  base = 10**(k-1)
+  for (i, x in enumerate(s)) {
+      if (i-k >= 0) {
+          curr -= s[i-k]*base
+      curr = curr*10+x
+      if (i+1 >= k) {
+          result += int(curr && num%curr == 0)
+  return result;
+}
+
+export { divisorSubstrings };
