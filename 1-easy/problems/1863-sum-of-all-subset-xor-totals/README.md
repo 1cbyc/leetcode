580a11fb95110d14ca5e4d54625ad0b9a7d05b18
1cbyc <isaacnsisong@gmail.com>
2026-06-14 14:41:39 +0100

    Add 1-easy/problems/1863-sum-of-all-subset-xor-totals solutions

---
+++ 1-easy/problems/1863-sum-of-all-subset-xor-totals/README.md
@@ -0,0 +1,5 @@
+# 1863. Sum of All Subset XOR Totals
+
+- LeetCode: https://leetcode.com/problems/sum-of-all-subset-xor-totals/
+- Imported from easy backlog
+- Languages: cpp, python, typescript
---
+++ 1-easy/problems/1863-sum-of-all-subset-xor-totals/solution.ts
@@ -0,0 +1,16 @@
+function subsetXORSum(nums: any): boolean | number | string | any {
+  """
+  :type nums: List[int]
+  :rtype: int
+  """
+  # given there are k (k >= 1) nums of which ith bit is 1,
+  # the bit contributes to sum is:
+  # (nCr(k, 1) + nCr(k, 3) + ...) * (nCr(n - k, 0) + nCr(n - k, 1) + ...) * 2^i
+  # = 2^(k-1) * 2^(n-k) = 2^(n-1) * 2^i
+  result = 0
+  for (x in nums) {
+      result |= x
+  return result * 2**((nums).length-1);
+}
+
+export { subsetXORSum };
