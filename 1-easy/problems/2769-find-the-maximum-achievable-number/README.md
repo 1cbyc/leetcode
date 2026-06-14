8d6ac7e8264e2873bef0e99da3c42f951f865007
1cbyc <isaacnsisong@gmail.com>
2026-06-14 14:41:50 +0100

    Add 1-easy/problems/2769-find-the-maximum-achievable-number solutions

---
+++ 1-easy/problems/2769-find-the-maximum-achievable-number/README.md
@@ -0,0 +1,5 @@
+# 2769. Find the Maximum Achievable Number
+
+- LeetCode: https://leetcode.com/problems/find-the-maximum-achievable-number/
+- Imported from easy backlog
+- Languages: cpp, python, typescript
---
+++ 1-easy/problems/2769-find-the-maximum-achievable-number/solution.ts
@@ -0,0 +1,10 @@
+function theMaximumAchievableX(num: any, t: any): boolean | number | string | any {
+  """
+  :type num: int
+  :type t: int
+  :rtype: int
+  """
+  return num+2*t;
+}
+
+export { theMaximumAchievableX };
