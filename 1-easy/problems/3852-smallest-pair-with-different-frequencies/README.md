9598de66957f5e58089b0a277b0f96c2ff692732
1cbyc <isaacnsisong@gmail.com>
2026-06-14 14:42:04 +0100

    Add 1-easy/problems/3852-smallest-pair-with-different-frequencies solutions

---
+++ 1-easy/problems/3852-smallest-pair-with-different-frequencies/README.md
@@ -0,0 +1,5 @@
+# 3852. Smallest Pair With Different Frequencies
+
+- LeetCode: https://leetcode.com/problems/smallest-pair-with-different-frequencies/
+- Imported from easy backlog
+- Languages: cpp, python, typescript
---
+++ 1-easy/problems/3852-smallest-pair-with-different-frequencies/solution.ts
@@ -0,0 +1,18 @@
+function minDistinctFreqPair(nums: any): boolean | number | string | any {
+  """
+  :type nums: List[int]
+  :rtype: List[int]
+  """
+  INF = float("inf")
+  cnt = collections.defaultdict(int)
+  for (x in nums) {
+      cnt[x] += 1
+  x = min(nums)
+  mn = INF
+  for (y in nums) {
+      if (cnt[y] != cnt[x]) {
+          mn = min(mn, y)
+  return [x, mn] if mn is not INF else [-1, -1];
+}
+
+export { minDistinctFreqPair };
