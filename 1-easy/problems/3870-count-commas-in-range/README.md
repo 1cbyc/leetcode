215b3e658c0007a34524a52e280aeea386c857b1
1cbyc <isaacnsisong@gmail.com>
2026-06-14 14:42:05 +0100

    Add 1-easy/problems/3870-count-commas-in-range solutions

---
+++ 1-easy/problems/3870-count-commas-in-range/README.md
@@ -0,0 +1,5 @@
+# 3870. Count Commas in Range
+
+- LeetCode: https://leetcode.com/problems/count-commas-in-range/
+- Imported from easy backlog
+- Languages: cpp, python, typescript
---
+++ 1-easy/problems/3870-count-commas-in-range/solution.ts
@@ -0,0 +1,17 @@
+function countCommas(n: any): boolean | number | string | any {
+  """
+  :type n: int
+  :rtype: int
+  """
+  cnt, base = 0, 1
+  while (base*1000 <= n) {
+      base *= 1000
+      cnt += 1
+  result, base = 0, 1
+  for (i in Array.from({length: cnt}, (_, i) => i)) {
+      base *= 1000
+      result += n-base+1
+  return result;
+}
+
+export { countCommas };
