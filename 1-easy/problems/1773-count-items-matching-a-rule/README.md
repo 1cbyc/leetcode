6cec316a8967efec7151bb134d7fbc60f18c77bf
1cbyc <isaacnsisong@gmail.com>
2026-06-14 14:41:38 +0100

    Add 1-easy/problems/1773-count-items-matching-a-rule solutions

---
+++ 1-easy/problems/1773-count-items-matching-a-rule/README.md
@@ -0,0 +1,5 @@
+# 1773. Count Items Matching a Rule
+
+- LeetCode: https://leetcode.com/problems/count-items-matching-a-rule/
+- Imported from easy backlog
+- Languages: cpp, python, typescript
---
+++ 1-easy/problems/1773-count-items-matching-a-rule/solution.py
@@ -0,0 +1,12 @@
+from typing import List, Optional, Dict, Set, Tuple
+
+class Solution:
+    def countMatches(self, items, ruleKey, ruleValue):
+        """
+        :type items: List[List[str]]
+        :type ruleKey: str
+        :type ruleValue: str
+        :rtype: int
+        """
+        rule = {"type":0, "color":1, "name":2}
+        return sum(item[rule[ruleKey]] == ruleValue for item in items)
