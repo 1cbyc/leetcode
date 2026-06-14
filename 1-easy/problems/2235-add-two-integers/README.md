5cd70fa2462a0c84d3a1759e34c5cd5d72d34070
1cbyc <isaacnsisong@gmail.com>
2026-06-14 14:41:43 +0100

    Add 1-easy/problems/2235-add-two-integers solutions

---
+++ 1-easy/problems/2235-add-two-integers/README.md
@@ -0,0 +1,5 @@
+# 2235. Add Two Integers
+
+- LeetCode: https://leetcode.com/problems/add-two-integers/
+- Imported from easy backlog
+- Languages: cpp, python, typescript
---
+++ 1-easy/problems/2235-add-two-integers/solution.ts
@@ -0,0 +1,10 @@
+function sum(num1: any, num2: any): boolean | number | string | any {
+  """
+  :type num1: int
+  :type num2: int
+  :rtype: int
+  """
+  return num1+num2;
+}
+
+export { sum };
