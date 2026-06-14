e05aae6728ed1b966f2bc9f44df323b66f73ec04
1cbyc <isaacnsisong@gmail.com>
2026-06-14 14:41:29 +0100

    Add 1-easy/problems/0999-available-captures-for-rook solutions

---
+++ 1-easy/problems/0999-available-captures-for-rook/README.md
@@ -0,0 +1,5 @@
+# 999. Available Captures for Rook
+
+- LeetCode: https://leetcode.com/problems/available-captures-for-rook/
+- Imported from easy backlog
+- Languages: cpp, python, typescript
---
+++ 1-easy/problems/0999-available-captures-for-rook/solution.ts
@@ -0,0 +1,9 @@
+function numRookCaptures(board: any): boolean | number | string | any {
+  """
+  :type board: List[List[str]]
+  :rtype: int
+  """
+  directions = [(0, 1), (1, 0), (0, -1), (-1, 0)]
+}
+
+export { numRookCaptures };
