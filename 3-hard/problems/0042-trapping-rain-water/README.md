85b5c55b66ed1e97e3501c4e01cfa2179492bcb7
1cbyc <isaacnsisong@gmail.com>
2026-06-17 22:59:34 +0100

    Add 3-hard/problems/0042-trapping-rain-water solutions

---
+++ 3-hard/problems/0042-trapping-rain-water/README.md
@@ -0,0 +1,4 @@
+# 42. Trapping Rain Water
+
+- LeetCode: https://leetcode.com/problems/trapping-rain-water/
+- Languages: python
---
+++ 3-hard/problems/0042-trapping-rain-water/solution.py
@@ -0,0 +1,34 @@
+from typing import List
+
+
+class Solution:
+    def trap(self, height: List[int]) -> int:
+        if not height:
+            return 0
+
+        left, right = 0, len(height) - 1
+        left_max, right_max = height[left], height[right]
+        water = 0
+
+        while left < right:
+            if left_max <= right_max:
+                left += 1
+                left_max = max(left_max, height[left])
+                water += left_max - height[left]
+            else:
+                right -= 1
+                right_max = max(right_max, height[right])
+                water += right_max - height[right]
+
+        return water
+
+
+if __name__ == "__main__":
+    solution = Solution()
+
+    assert solution.trap([0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1]) == 6
+    assert solution.trap([4, 2, 0, 3, 2, 5]) == 9
+    assert solution.trap([]) == 0
+    assert solution.trap([1, 2, 3]) == 0
+
+    print("All tests passed!")
