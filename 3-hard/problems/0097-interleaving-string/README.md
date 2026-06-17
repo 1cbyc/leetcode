c144a64ce625e8cadda00e6565f94a0088b66d57
1cbyc <isaacnsisong@gmail.com>
2026-06-17 23:02:11 +0100

    Add 3-hard/problems/0097-interleaving-string solutions

---
+++ 3-hard/problems/0097-interleaving-string/README.md
@@ -0,0 +1,4 @@
+# 97. Interleaving String
+
+- LeetCode: https://leetcode.com/problems/interleaving-string/
+- Languages: python
---
+++ 3-hard/problems/0097-interleaving-string/solution.py
@@ -0,0 +1,30 @@
+class Solution:
+    def isInterleave(self, s1: str, s2: str, s3: str) -> bool:
+        m, n = len(s1), len(s2)
+        if m + n != len(s3):
+            return False
+
+        dp = [False] * (n + 1)
+        dp[0] = True
+
+        for j in range(1, n + 1):
+            dp[j] = dp[j - 1] and s2[j - 1] == s3[j - 1]
+
+        for i in range(1, m + 1):
+            dp[0] = dp[0] and s1[i - 1] == s3[i - 1]
+            for j in range(1, n + 1):
+                dp[j] = (dp[j] and s1[i - 1] == s3[i + j - 1]) or (
+                    dp[j - 1] and s2[j - 1] == s3[i + j - 1]
+                )
+
+        return dp[n]
+
+
+if __name__ == "__main__":
+    solution = Solution()
+
+    assert solution.isInterleave("aabcc", "dbbca", "aadbbcbcac") is True
+    assert solution.isInterleave("aabcc", "dbbca", "aadbbbaccc") is False
+    assert solution.isInterleave("", "", "") is True
+
+    print("All tests passed!")
